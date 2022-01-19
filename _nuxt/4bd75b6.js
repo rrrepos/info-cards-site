10fbbf70c4698d61171213b05ee730bde6953c17
RmR <connect2rmr@gmail.com>
2022-01-19 18:47:11 +0530

    update

--- _nuxt/4bd75b6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{224:function(e,n,l){"use strict";l.r(n);var t={name:"IndexPage"},c=l(51),component=Object(c.a)(t,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Hello")])}),[],!1,null,null,null);n.default=component.exports}}]);
