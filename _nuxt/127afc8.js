d8405fcc04dd5d07f38eeb0a0cb218b3d61a1606
RmR <connect2rmr@gmail.com>
2022-01-20 08:51:52 +0530

    copied all files

--- _nuxt/127afc8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{224:function(e,n,l){"use strict";l.r(n);var t={name:"IndexPage"},o=l(51),component=Object(o.a)(t,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Hello World")])}),[],!1,null,null,null);n.default=component.exports}}]);
