8484ee16fe3f2c552c51a2b7eae88563b410a5a4
RmR <connect2rmr@gmail.com>
2022-01-20 08:47:39 +0530

    changed title

---
+++ dist/_nuxt/79087d9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{224:function(e,n,t){"use strict";t.r(n);var r={name:"IndexPage"},l=t(51),component=Object(l.a)(r,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("This better work")])}),[],!1,null,null,null);n.default=component.exports}}]);
