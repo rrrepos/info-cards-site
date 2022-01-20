bc49f4fa7040abb38631389cccc4fba387cbb4f4
RmR <connect2rmr@gmail.com>
2022-01-20 08:59:31 +0530

    added two pages

---
+++ _nuxt/80b998c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{226:function(e,n,t){"use strict";t.r(n);var l=t(42),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Page 1")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1642649349/page2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Finfo-cards-site\u002F_nuxt\u002Fstatic\u002F1642649349",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpage2",config:{_app:{basePath:"\u002Finfo-cards-site\u002F",assetsPath:"\u002Finfo-cards-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
