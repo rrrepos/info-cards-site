78886a6c5a812c01d1aaba0636e953fad427b4f3
RmR <connect2rmr@gmail.com>
2022-01-24 13:33:19 +0530

    changed config

---
+++ _nuxt/static/1643011334/preview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Finfo-cards-site\u002F_nuxt\u002Fstatic\u002F1643011334",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpreview",config:{_app:{basePath:"\u002Finfo-cards-site\u002F",assetsPath:"\u002Finfo-cards-site\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"950cb27b"}}}}(null));
