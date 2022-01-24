6c50fc1fe22d556a390d6ed4198af7148913ba66
RmR <connect2rmr@gmail.com>
2022-01-24 13:59:36 +0530

    all routes generation

---
+++ _nuxt/static/1643012787/decks/what_is_bitcoin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Finfo-cards-site\u002F_nuxt\u002Fstatic\u002F1643012787",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_bitcoin",config:{_app:{basePath:"\u002Finfo-cards-site\u002F",assetsPath:"\u002Finfo-cards-site\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4dd27eac"}}}}(null));
