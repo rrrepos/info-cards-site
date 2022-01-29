2ca3195a522fb02a7e6b445d5f31ea3551b3765a
RmR <connect2rmr@gmail.com>
2022-01-29 19:51:10 +0530

    reverted to click

---
+++ _nuxt/static/1643466019/decks/what_is_nft/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643466019",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_nft",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12d9da56"}}}}(null));
