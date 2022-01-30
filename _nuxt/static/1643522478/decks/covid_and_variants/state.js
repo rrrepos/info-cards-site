373ba54d2bf351988256e12586490bd5c5ea306b
RmR <connect2rmr@gmail.com>
2022-01-30 11:32:58 +0530

    reverted to v2-touch with reverse

---
+++ _nuxt/static/1643522478/decks/covid_and_variants/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643522478",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fcovid_and_variants",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"30087489"}}}}(null));
