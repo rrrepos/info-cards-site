8dd5b0134a5431ae42f139318caf55708c99120e
RmR <connect2rmr@gmail.com>
2022-01-30 11:35:41 +0530

    change body css

---
+++ _nuxt/static/1643522718/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdecks\u002Fwhat_is_nft","\u002Fdecks\u002Fwhat_is_blockchain_ledger","\u002Fdecks\u002Fwhat_is_bitcoin","\u002Fdecks\u002Fcovid_and_variants"]})
