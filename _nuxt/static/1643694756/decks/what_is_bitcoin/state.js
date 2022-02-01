1a48661bc59ad5d13452e4c13fb8051dd623efa9
RmR <connect2rmr@gmail.com>
2022-02-01 11:22:49 +0530

    updated metaverse

---
+++ _nuxt/static/1643694756/decks/what_is_bitcoin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643694756",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_bitcoin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0c6e6ae1"}}}}(null));
