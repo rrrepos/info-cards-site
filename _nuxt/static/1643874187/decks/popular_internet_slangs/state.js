00185e26128674c6f4c7b33d4aca5063cf52c762
RmR <connect2rmr@gmail.com>
2022-02-03 13:15:45 +0530

    added popular_internet_slangs

---
+++ _nuxt/static/1643874187/decks/popular_internet_slangs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643874187",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fpopular_internet_slangs",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d437ba30"}}}}(null));
