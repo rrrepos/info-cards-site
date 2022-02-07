a0027dda4e3ba632a4c3158995d2d8f59140aea9
RmR <connect2rmr@gmail.com>
2022-02-07 10:24:26 +0530

    added AR

---
+++ _nuxt/static/1644209574/decks/what_is_blockchain_ledger/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644209574",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_blockchain_ledger",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"53db1c5e"}}}}(null));
