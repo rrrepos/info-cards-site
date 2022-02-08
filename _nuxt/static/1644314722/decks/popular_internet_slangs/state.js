bdf746b11037033b65d4a605c4bbdc2bdaa0f4bc
RmR <connect2rmr@gmail.com>
2022-02-08 15:35:46 +0530

    added quantum computers

---
+++ _nuxt/static/1644314722/decks/popular_internet_slangs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644314722",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fpopular_internet_slangs",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"06f192c6"}}}}(null));
