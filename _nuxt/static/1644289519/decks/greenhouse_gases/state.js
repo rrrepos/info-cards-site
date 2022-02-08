7dec1cb50b50eeb9f3272bbe4fea397932ade489
RmR <connect2rmr@gmail.com>
2022-02-08 08:36:41 +0530

    regenerated for 7-Feb

---
+++ _nuxt/static/1644289519/decks/greenhouse_gases/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644289519",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fgreenhouse_gases",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed86d1e4"}}}}(null));
