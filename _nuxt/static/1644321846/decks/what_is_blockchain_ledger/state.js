da546276d5ec1ef4e6146445d24914a0b2c6e87f
RmR <connect2rmr@gmail.com>
2022-02-08 17:34:45 +0530

    added cbdc

---
+++ _nuxt/static/1644321846/decks/what_is_blockchain_ledger/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644321846",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_blockchain_ledger",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b22cf972"}}}}(null));
