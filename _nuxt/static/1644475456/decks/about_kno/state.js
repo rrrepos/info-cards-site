2d7aafdb8fda5f6ca4ce45c524cf4ccc6e398d59
RmR <connect2rmr@gmail.com>
2022-02-10 12:16:28 +0530

    set for pwa

---
+++ _nuxt/static/1644475456/decks/about_kno/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644475456",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fabout_kno",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3c28970"}}}}(null));
