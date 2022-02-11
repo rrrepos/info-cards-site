620d977ef4bb698d287b1f1c3d62e4a4aae2c064
RmR <connect2rmr@gmail.com>
2022-02-11 17:52:28 +0530

    added robotic surgery

---
+++ _nuxt/static/1644582064/decks/what_is_wordle/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644582064",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_wordle",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b5ff30e8"}}}}(null));
