a3eb78088b4fa1b784a9579e5abf3e39ff3190ba
RmR <connect2rmr@gmail.com>
2022-02-11 11:41:07 +0530

    changed google analytics settings

---
+++ _nuxt/static/1644559819/decks/what_is_vr/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644559819",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_vr",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b0d1819"}}}}(null));
