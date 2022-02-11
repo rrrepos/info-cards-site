c3ab86e67b7458828ca997a4fd45ecb2816bed86
RmR <connect2rmr@gmail.com>
2022-02-11 14:21:26 +0530

    enabled pwa reload on change

---
+++ _nuxt/static/1644569446/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644569446",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fb96f332"}}}}(null,"\u002F"));
