78ba156d0aaf66cc9a7c329f7245ee7a47ef8451
RmR <connect2rmr@gmail.com>
2022-02-13 16:19:16 +0530

    added quad

---
+++ _nuxt/static/1644749267/decks/malaria_vaccine/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644749267",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fmalaria_vaccine",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d06355b"},http:{browserBaseURL:b}}}}(null,"\u002F"));
