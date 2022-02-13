6c94d90cf16ff424a3369b336d3f62a20c549c62
RmR <connect2rmr@gmail.com>
2022-02-13 11:41:30 +0530

    added ratings and error reporting

---
+++ _nuxt/static/1644732542/decks/what_is_bitcoin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644732542",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdecks\u002Fwhat_is_bitcoin",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"454c9685"},http:{browserBaseURL:b}}}}(null,"\u002F"));
