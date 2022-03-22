a15bc06a43c3b82be2dcd9df2e6c7e729240c46f
RmR <connect2rmr@gmail.com>
2022-03-22 08:26:59 +0530

    added bhai-lang

---
+++ _nuxt/b368e0f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{597:function(t,n,c){},603:function(t,n,c){"use strict";c(597)},609:function(t,n,c){"use strict";c.r(n);var e={},o=(c(603),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
