88a6a75ba493e6509df7d326376f0b6bbf106cfb
RmR <connect2rmr@gmail.com>
2022-03-24 18:20:12 +0530

    added sensors on breeze

---
+++ _nuxt/a78f22e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{605:function(t,n,c){},611:function(t,n,c){"use strict";c(605)},617:function(t,n,c){"use strict";c.r(n);var e={},o=(c(611),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
