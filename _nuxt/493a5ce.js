3a0d526574fe5e3f3ae93ffb5caaad45a709ec22
RmR <connect2rmr@gmail.com>
2022-04-14 11:25:39 +0530

    added motion_detectors

---
+++ _nuxt/493a5ce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{659:function(t,n,c){},665:function(t,n,c){"use strict";c(659)},671:function(t,n,c){"use strict";c.r(n);var e={},o=(c(665),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
