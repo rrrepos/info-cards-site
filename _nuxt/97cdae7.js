ccfa6ae77413b8db055de2bfb9babdf7909e0fb6
RmR <connect2rmr@gmail.com>
2022-05-02 12:00:15 +0530

    added flexible_speakers

---
+++ _nuxt/97cdae7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{717:function(t,n,c){},728:function(t,n,c){"use strict";c(717)},742:function(t,n,c){"use strict";c.r(n);var e={},o=(c(728),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
