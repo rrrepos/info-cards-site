8c4772b30e6931108691f1c78d1c9832b95e7e25
RmR <connect2rmr@gmail.com>
2022-05-04 09:33:39 +0530

    added transparent_solar_panels

---
+++ _nuxt/072a976.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{723:function(t,n,c){},734:function(t,n,c){"use strict";c(723)},748:function(t,n,c){"use strict";c.r(n);var e={},o=(c(734),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
