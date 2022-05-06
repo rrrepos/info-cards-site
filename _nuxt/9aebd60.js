cbe3b6eda52c8bae640b6af839eb2554768d61b3
RmR <connect2rmr@gmail.com>
2022-05-06 12:33:09 +0530

    added gait_recognition

---
+++ _nuxt/9aebd60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{729:function(t,n,c){},741:function(t,n,c){"use strict";c(729)},756:function(t,n,c){"use strict";c.r(n);var e={},o=(c(741),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
