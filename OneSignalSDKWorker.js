0b3677e1f9ae1e2fd5e7af3b63db2255c6cd4316
RmR <connect2rmr@gmail.com>
2022-06-06 09:47:58 +0530

    added biometric_passport

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1653377131456', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1654488995629', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
---
+++ _nuxt/706a00d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{759:function(t,n,c){},771:function(t,n,c){"use strict";c(759)},787:function(t,n,c){"use strict";c.r(n);var e={},o=(c(771),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
