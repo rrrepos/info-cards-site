c939b0a8227c56a8fb6e899956816bbf96aa5619
RmR <connect2rmr@gmail.com>
2022-06-06 13:26:50 +0530

    added bionic_reading

---
+++ _nuxt/static/1654502170/list/business/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/list/business", {data:[{decks:[{id:"poison_pills",title:"Poison Pills",description:"Understand different Poison Pill Strategies",tags:["business"],listable:true,dir:"\u002Fdecks\u002Fen\u002Fpoison_pills",createdAt:"2022-04-23T10:07:42.967Z"}]}],fetch:{},mutations:[]});
