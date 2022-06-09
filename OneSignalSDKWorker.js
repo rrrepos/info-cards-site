84ecf7985902ade97e32218c04a322d7e367f3b4
RmR <connect2rmr@gmail.com>
2022-06-09 09:09:42 +0530

    added sbsp

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1654667646740', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1654745897028', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
---
+++ _nuxt/6909e6b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{774:function(t,n,c){},786:function(t,n,c){"use strict";c(774)},802:function(t,n,c){"use strict";c.r(n);var e={},o=(c(786),c(39)),component=Object(o.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"container"})}),[],!1,null,"532500c8",null);n.default=component.exports}}]);
